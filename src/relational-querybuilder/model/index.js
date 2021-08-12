c7c8c8dcd9b2868ce846595ec558db032efe351e
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-08-12 19:34:14 +0200

    Add more exports to relational querybuilder model index

--- src/relational-querybuilder/model/index.js
+++ src/relational-querybuilder/model/index.js
@@ -1,7 +1,11 @@
 const Department = require('./department');
 const Employee = require('./employee');
+const Salary = require('./salary');
+const Title = require('./title');
 
 module.exports = {
   Department,
   Employee,
+  Salary,
+  Title
 };
