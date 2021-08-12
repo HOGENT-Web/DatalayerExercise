0f7f8db3599165eb82496ad6f2b5e4ea0b57ed95
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-08-12 18:02:36 +0200

    Add knex exercises

--- src/querybuilder/exercises.js
+++ src/querybuilder/exercises.js
@@ -3,6 +3,21 @@ const getConnection = require('./_connection');
 const main = async () => {
   const knex = await getConnection();
 
+  // 1. Get all departments
+  const departments = []; // TODO: query here
+  console.log('EXERCISE 1\n----------');
+  console.table(departments);
+
+  // 2. Get all managers of each department (order by from_date and then to_date)
+  const managers = []; // TODO: query here
+  console.log('\nEXERCISE 2\n----------');
+  console.table(managers);
+
+  // 3. Get the salaries of the managers from exercise 2
+  const salaries = []; // TODO: query here
+  console.log('\nEXERCISE 3\n----------');
+  console.table(salaries);
+
   // Close the connection
   await knex.destroy();
 };
